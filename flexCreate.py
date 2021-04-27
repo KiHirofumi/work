7fe85d05be06c376ce8b99d96b82281677463bd7
unknown <vocaloid.chibakun.mana@gmail.com>
2021-04-27 21:37:38 +0900

    引越しメニュー2-15.2

--- flexCreate.py
+++ flexCreate.py
@@ -5,5 +5,5 @@ import json
 
 def make_flex():
     with open('hikkosimenu.txt') as flex_message_json_string:
-        flex_message_json_dict_hikkosi =  json.loads(flex_message_json_string)
+        flex_message_json_dict_hikkosi =  json.load(flex_message_json_string)
         return flex_message_json_dict_hikkosi
